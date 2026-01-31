c70b61da56516de0145160a371d05663b952ffe9
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-01-31 13:21:08 +0530

    Time: 3 ms (31.63%) | Memory: 10 MB (42.80%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result = {{}};
+
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                vector<int> temp = result[i];
+                temp.push_back(num);
+                result.push_back(temp);
+            }
+        }
+
+        return result;
+    }
+};
