653d31b8ab1fb149fbc29e5e2c7bc921f767d5df
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-03-18 22:06:51 +0530

    Time: 1 ms (75.21%) | Memory: 23.5 MB (44.49%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.cpp
+++ 860-design-circular-queue/design-circular-queue.cpp
@@ -2,15 +2,13 @@ class MyCircularQueue {
 public:
     vector<int> q;
     int front, rear, size, capacity;
-
-    MyCircularQueue(int k) {
+     MyCircularQueue(int k) {
         capacity = k;
         q.resize(k);
         front = 0;
         rear = -1;
         size = 0;
     }
-    
     bool enQueue(int value) {
         if(isFull()) return false;
         rear = (rear + 1) % capacity;
@@ -18,28 +16,23 @@ public:
         size++;
         return true;
     }
-    
     bool deQueue() {
         if(isEmpty()) return false;
         front = (front + 1) % capacity;
         size--;
         return true;
     }
-    
-    int Front() {
+     int Front() {
         if(isEmpty()) return -1;
         return q[front];
     }
-    
-    int Rear() {
+     int Rear() {
         if(isEmpty()) return -1;
         return q[rear];
     }
-    
     bool isEmpty() {
         return size == 0;
     }
-    
     bool isFull() {
         return size == capacity;
     }
