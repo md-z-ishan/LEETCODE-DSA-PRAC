273cb2aef883444bb9cb83a0537ae36a0c371352
Md Zishan <150007240+md-z-ishan@users.noreply.github.com>
2026-08-30 04:53:18 +0530

    Time: 3 ms (92.75%), Space: 46.5 MB (88.61%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        boolean[] used = new boolean[nums.length];
+        
+        solve(nums, used, new ArrayList<>(), ans);
+        
+        return ans;
+    }
+
+    void solve(int[] nums, boolean[] used, 
+               List<Integer> temp, List<List<Integer>> ans) {
+        
+        if(temp.size() == nums.length) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            
+            if(used[i])
+                continue;
+
+            if(i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+                continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            solve(nums, used, temp, ans);
+
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
+}
